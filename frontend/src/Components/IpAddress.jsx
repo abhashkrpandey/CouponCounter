4fcd3e8ac28e191ac876d4f07360f9e471a31fc0
Abhash Pandey <abhashkrpandey4@gmail.com>
2025-03-16 00:40:26 +0530

    deployed again

--- frontend/src/Components/IpAddress.jsx
+++ frontend/src/Components/IpAddress.jsx
@@ -1,6 +1,6 @@
 export default function IpAddress({ ipaddress, sessionId, coupon }) {
     return (
-        <div className="bg-amber-400 flex flex-col w-[21rem] h-[73px] text-white text-[16px] font-bold rounded-[2px]">
+        <div className="bg-amber-400 flex flex-col w-[23rem] h-[73px] text-white text-[16px] font-bold rounded-[2px]">
             <div>
                 CouponId:{coupon}
             </div>
